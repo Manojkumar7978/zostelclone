524e5528362619968487e460c0356428947c7bb9
Manoj Kumar Padhy <manojkumarpadhyms@gmail.com>
2023-04-21 23:32:19 +0530

    day4

--- src/component/latestreads.jsx
+++ src/component/latestreads.jsx
@@ -1,9 +1,13 @@
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
 
 const Latestreads = () => {
     return (
         <div>
             xxx
+            <Flex>
+                asdfkjsdfkjsadfj
+            </Flex>
         </div>
     );
 }
